64d46c85f4416797beac13c3e2e9b8d4896cd32f
root <root@C7-devoses01.soprote.net>
2017-02-08 17:19:27 +0200

    Testing

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,12 @@
 from flask import Flask
+import socket
+
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World!"
+    abc = socket.gethostbyname('12345678.lev.iot.ilab.cloud')
+    return abc
 
 if __name__ == "__main__":
     application.run()
