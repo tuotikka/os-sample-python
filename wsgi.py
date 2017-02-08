76e103c7d3a6ca0236d6275cce1f3ffa22fc93c7
root <root@C7-devoses01.soprote.net>
2017-02-08 20:42:36 +0200

    testing

--- wsgi.py
+++ wsgi.py
@@ -6,7 +6,7 @@ application = Flask(__name__)
 @application.route("/")
 def hello():
     abc = socket.gethostbyname('12345678.lev.iot.ilab.cloud')
-    return abc
+    return "raidai " + abc
 
 if __name__ == "__main__":
     application.run()
