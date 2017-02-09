3f93419c518d84f6f3438951907724f754f669c6
root <root@C7-devoses01.soprote.net>
2017-02-09 15:29:48 +0200

    Testing

--- wsgi.py
+++ wsgi.py
@@ -6,10 +6,10 @@ application = Flask(__name__)
 @application.route("/")
 def hello():
     try:
-      abc = socket.gethostbyname('12345678.lev.iot.ilab.cloud')
+      abc = gethostbyname('12345678.lev.iot.ilab.cloud')
     except gaierror:
       abc = "Address not found"
-    except:
+    except e:
       abc = "General Error 1"
 
     return abc
